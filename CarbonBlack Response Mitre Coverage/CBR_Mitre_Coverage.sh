f22ad6a79213fe3979d3971bf70c7fcc4521e0ad
alibay7 <37883006+alibay7@users.noreply.github.com>
2019-07-31 03:03:15 +0300

    Auto update the actual number of ATT&CK Techniques

--- CarbonBlack Response Mitre Coverage/CBR_Mitre_Coverage.sh
+++ CarbonBlack Response Mitre Coverage/CBR_Mitre_Coverage.sh
@@ -49,13 +49,19 @@ EOF
 
 function main() {
 	banner
+
+	curl https://attack.mitre.org/ >mitre.txt 2>&1
+	mitre=$(grep -io "t[0-9][0-9][0-9][0-9]" mitre.txt  |sed -e 's/^\(.\)/\U\1/g' |sort | uniq |wc -l)  # Total Number of MITRE ATT&CK Techniques
+
+
 	curl -XGET -H "X-Auth-Token: $token" -H "Content-Type: application/json" "$url/api/v1/threat_report?cb.urlver=1&cb.fq.feed_name=attackframework&cb.fq.feed_name=bit9advancedthreats&cb.fq.feed_name=cbcommunity&cb.fq.feed_name=sans&cb.fq.feed_name=bit9endpointvisibility&cb.fq.feed_name=bit9suspiciousindicators&cb.fq.feed_name=bit9earlyaccess&sort=severity_score%20desc&rows=50000&facet=false&start=0&cb.fq.is_deleted=false" -k > out.txt 2>&1
 	RESULT=$(grep -io "t[0-9][0-9][0-9][0-9]" out.txt  |sed -e 's/^\(.\)/\U\1/g' |sort | uniq | wc -l)
 	
 	rm -rf out.txt				# Delete Output File
+	rm -rf mitre.txt			# Delete Output File
 
 	echo "###########################################################################################################"
-	echo "############       $RESULT out of 224 MITRE ATT&CK Techniques Covered by CarbonBlack Response      ############"
+	echo "############       $RESULT out of $mitre MITRE ATT&CK Techniques Covered by CarbonBlack Response      ############"
 	echo "###########################################################################################################"
 }
 
